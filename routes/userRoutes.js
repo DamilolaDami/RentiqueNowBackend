d0067c89674e3268eb200cc2494bc19fbb5fa56c
Damilola <kabirabdul08091@gmaail.com>
2024-09-16 23:12:11 +0100

    change db url

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -30,11 +30,11 @@ router.get('/:id', async (req, res) => {
 
 // POST a new user
 router.post('/', async (req, res) => {
-  const { name, email, pushNotificationToken, userType, uid } = req.body;
+  const { name, email, pushNotificationToken, userType, uid, photoUrl, phoneNumber } = req.body;
   try {
     const result = await db.query(
-      'INSERT INTO users (name, email, pushNotificationToken, userType, uid) VALUES ($1, $2, $3, $4, $5) RETURNING *',
-      [name, email, pushNotificationToken, userType, uid]
+      'INSERT INTO users (name, email, pushNotificationToken, userType, uid, photoUrl, phoneNumber) VALUES ($1, $2, $3, $4, $5, $6, $7) RETURNING *',
+      [name, email, pushNotificationToken, userType, uid, photoUrl, phoneNumber]
     );
     res.status(201).json(result.rows[0]);
   } catch (err) {
